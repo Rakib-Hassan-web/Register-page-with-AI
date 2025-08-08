28659172723f9fa5be3c7e43e67e7c0eb05be017
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-08-09 00:43:35 +0600

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
+import Home from './Components/Home'
 
 function App() {
+
+  const RountMy =createBrowserRouter(createRoutesFromElements(
+
+
+<Route>
+  <Route path='/' element={<Home/>}/>
+</Route>
+
+
+  ))
 
 
   return (
     <>
     
-
-
+  <RouterProvider router={RountMy}/>
 
 
 
